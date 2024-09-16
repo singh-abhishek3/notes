cb59265a8d83d9df7c51080ddf2f0efe37cc0241
Abhishek Singh <asing644@asu.edu>
2024-09-16 15:50:27 +0530

    Initial commit

--- sst.config.ts
+++ sst.config.ts
@@ -1,19 +1,15 @@
-/// <reference path="./.sst/platform/config.d.ts" />
+// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
   app(input) {
     return {
-      name: "monorepo-template",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      home: "aws",
+      name: 'notes',
+      removal: input?.stage === 'production' ? 'retain' : 'remove',
+      home: 'aws',
     };
   },
   async run() {
-    await import("./infra/storage");
-    const api = await import("./infra/api");
-
-    return {
-      api: api.myApi.url,
-    };
+    await import('./infra/storage');
+    await import('./infra/api');
   },
 });
