eff05b4f422d08abf1795b43fc8ff32f046dee77
Abhishek Singh <asing644@asu.edu>
2024-09-17 17:02:50 +0530

    Secuting the API

--- infra/api.ts
+++ infra/api.ts
@@ -7,6 +7,12 @@ export const api = new sst.aws.ApiGatewayV2('Api', {
       handler: {
         link: [table],
       },
+      args: {
+        auth: { iam: true },
+      },
+    },
+    args: {
+      auth: { iam: true },
     },
   },
 });
